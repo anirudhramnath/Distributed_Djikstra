99d20101cbd1b5c515a1bc5b482c59062d603cec
anirudhramnath <ani@dhcp-vlan876-9-217-95.wireless.uic.edu>
2015-02-18 14:43:35 -0600

    removed some print statements

--- src/edu/uic/ids561/aramna2/DijkstraPartitioner.java
+++ src/edu/uic/ids561/aramna2/DijkstraPartitioner.java
@@ -18,17 +18,14 @@ public class DijkstraPartitioner implements Partitioner<LongWritable, Node> {
 			return 0;
 		}
 		else{
-			// this will map the intermediate key-val pairs among 3 reducers
+			// this will map the intermediate key-value pairs among 3 reducers
 			if( key.get() % 3 == 0 ){
-				System.out.println("Reducer 0");
 				return 0;
 			}
 			else if( key.get() % 3 == 1 ){
-				System.out.println("Reducer 2");
 				return 1 % numOfReduceTasks;
 			}
 			else{
-				System.out.println("Reducer 1");
 				return 2 % numOfReduceTasks;
 			}
 		}
